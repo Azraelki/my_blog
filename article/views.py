dfafdc4f5f25b2b2cbbfcd411fb396866f3bd0b0
Azraelki <584917180@qq.com>
2017-07-08 17:38:42 +0800

    django tutorial init

---
+++ article/views.py
@@ -0,0 +1,16 @@
+# coding:utf-8
+from django.shortcuts import render
+from django.http import HttpResponse
+from article.models import Article
+from datetime import datetime
+from django.http import Http404
+
+
+def home(request):
+    post_list = Article.objects.all()
+    return render(request, 'home.html', {'post_list': post_list})
+
+
+def detail(request, my_args):
+    post = Article.objects.get(id=int(my_args))
+    return render(request, 'post.html', {'post': post})
